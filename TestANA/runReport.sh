5feddd0df756ffad7187318d465335c7cc8823a7
Matrixtang <matrixkook@outlook.com>
2022-04-28 19:36:10 +0800

    [Update]: use lit-rs to test all. sh

---
+++ TestANA/runReport.sh
@@ -0,0 +1,5 @@
+# RUN: sh @file
+/Volumes/CODE/llvm-project/build/bin/clang++ -cc1  \
+-load build/lib/clang-plugin/ANATestPlugin.dylib \
+-analyze \
+-analyzer-checker=example.MainCallChecker TestANA/testcase/sampleChecker/sampleChecker.c -o build/report
